69b44d21107939b1fe2c011d7798bb0c14ceb4f4
Dylan Boland <dylan.boland@ucdconnect.ie>
2023-05-13 15:13:54 +0100

    Updating the testbench to be more flexible; updated the design RTL so that if the multiplier (number on the bottom) is a negative number, the last partial product is taken to be the complement of the multiplicand (number on the top), shifted over by the correct amount - now all of the tests are passing; also adding an annotated waveform showing some example calculations - this is located in the diagrams directory

--- TB_multiplier.v
+++ TB_multiplier.v
@@ -7,7 +7,12 @@ module TB_multiplier;
 	localparam WIDTH_B = 8;                 // number of bits in input b
 	localparam WIDTH_C = WIDTH_A + WIDTH_B; // number of bits in the product of a and b
 	localparam CLK_PERIOD = 30;             // clock period in ns
-
+	localparam MAX_VAL_A = {1'b0, {(WIDTH_A-1){1'b1}}};
+	localparam MAX_VAL_B = {1'b0, {(WIDTH_B-1){1'b1}}};
+	localparam MIN_VAL_A = {1'b1, {(WIDTH_A-1){1'b0}}};
+	localparam MIN_VAL_B = {1'b1, {(WIDTH_B-1){1'b0}}};
+	localparam DEBUG = 1;
+	
 	// ==== TB Signals ====
 	// Inputs
 	reg clk;
@@ -21,10 +26,13 @@ module TB_multiplier;
 	// ==== Counter Variables ====
 	integer error_cnt = 0;
 	integer pass_cnt = 0;
+	int num_tests = 5; // the number of times the module inputs are changed
 	
 	// ==== Test bench Variables Used during Randomisation ====
 	bit [WIDTH_A-1:0] num1;
 	bit [WIDTH_B-1:0] num2;
+	bit a_is_neg;
+	bit b_is_neg;
 
 	// ==== Instantiate the Design Under Test (DUT) ====
 	multiplier #
@@ -64,9 +72,12 @@ module TB_multiplier;
 	endtask
 	
 	// Function to return the Product of two Numbers
-	function int multiply (input int num1, num2);
+	function int multiply (input [WIDTH_A-1:0] num1, [WIDTH_B-1:0] num2);
+		int int1, int2;
+		int1 = $signed(num1);
+		int2 = $signed(num2);
 		begin
-			multiply = num1*num2;
+			multiply = int1*int2;
 		end
 	endfunction
 
@@ -83,13 +94,19 @@ module TB_multiplier;
 			#1;
 			a = num1;
 			b = num2;
+			if (DEBUG == 1) begin
+				$display("(a = %d, b = %d)", $signed(a), $signed(b));
+			end
 			@(posedge clk);
 			#1;
 			// ==== Check the Output ====
-			if (c == multiply(a, b)) begin
+			if ($signed(c) == multiply(a, b)) begin
 				pass_cnt = pass_cnt + 1;
 			end else begin
 				error_cnt = error_cnt + 1;
+			end
+			if (DEBUG == 1) begin
+				$display("(c = %d, multiply(a, b) = %d)", $signed(c), multiply(a, b));
 			end
 		end
 	endtask
@@ -104,13 +121,41 @@ module TB_multiplier;
 		// (2) Apply the reset
 		reset();
 		// (3) Drive the Input Signals to the DUT
-		repeat (10) begin
+		if ((num_tests == 0) | (num_tests < 0)) begin
+			num_tests = $urandom_range(32'd10, 32'd2);
+		end
+		repeat (num_tests) begin
 			// ==== Generate Some Random Numbers ====
-			num1 = $random;
-			num2 = $random;
+			num1 = $urandom_range(32'(MAX_VAL_A), 0);
+			num2 = $urandom_range(32'(MAX_VAL_B), 0);
+			// ==== Randomly decide if the Inputs should be made Negative ====
+			a_is_neg = $random;
+			b_is_neg = $random;
+			if (DEBUG == 1) begin
+				$display("(num1 = %d, num2 = %d)", $signed(num1), $signed(num2));
+			end
+			if (a_is_neg) begin
+				num1 = -1*num1;
+				if (DEBUG == 1) begin
+					$display("Input a will be negative (num1 = %d)", $signed(num1));
+				end
+			end
+			if (b_is_neg) begin
+				num2 = -1*num2;
+				if (DEBUG == 1) begin
+					$display("Input b will be negative (num2 = %d)", $signed(num2));
+				end
+			end
+			// ==== Update the Inputs and Check the Output ====
 			update_inputs_check_output(num1, num2);
 		end
+		#100;
+		// ==== Test the Module when the Inputs have their Maximum Values ====
+		update_inputs_check_output(MAX_VAL_A, MAX_VAL_B);
 		#50;
+		// ==== Test the Module when the Inputs have their Minimum Values ====
+		update_inputs_check_output(MIN_VAL_A, MIN_VAL_B);
+		#75;
 		// (4) Print a summary
 		print_summary();
 		$finish;
--- multiplier.v
+++ multiplier.v
@@ -19,9 +19,29 @@ module multiplier #
 	);
 
 	// ==== Internal Signals ====
+	wire [WIDTH_C-1:0] a_sign_extended;
+	wire [WIDTH_C-1:0] b_sign_extended;
+	wire [WIDTH_A:0] a_twos_complement;
+	wire [WIDTH_B:0] b_twos_complement;
+	wire a_is_negative; // 1-bit signal to indicate if input a is negative
+	wire b_is_negative; // and another 1-bit signal to indicate if input b is negative
 	reg [WIDTH_C-1:0] product_reg;
 	reg [WIDTH_C-1:0] product;
-
+	
+	// ==== Logic for Sign Extending the Inputs ====
+	assign a_sign_extended = {{(WIDTH_B){a[WIDTH_A-1]}}, a};
+	assign b_sign_extended = {{(WIDTH_A){b[WIDTH_B-1]}}, b};
+	
+	// ==== Logic for Forming the Two's complement of the Inputs a and b ====
+	assign a_twos_complement = ~{{a[WIDTH_A-1]}, a} + 1'b1;
+	assign b_twos_complement = ~{{b[WIDTH_B-1]}, b} + 1'b1;
+	
+	// ==== Logic for Sign-indicator Signals ====
+	// I am just adding these signals to try and improve
+	// the readability of the module.
+	assign a_is_negative = a[WIDTH_A-1]; // if the MSB is 1, then input a is negative, otherwise it's positive
+	assign b_is_negative = b[WIDTH_B-1];
+	
 	// ==== Logic for the Multiplication ====
 	always @ (*) begin
 		integer i; // variable for the for loop below
@@ -31,7 +51,14 @@ module multiplier #
 			// b the "multiplier" in order to minimise the number of partial
 			// products that we have to add together.
 			for (i = 0; i < WIDTH_B; i = i + 1) begin
-				product = product + ((a & {WIDTH_A{b[i]}}) << i);
+				if ((i == (WIDTH_B-1)) & b_is_negative) begin
+					// If b is a negative quantity (which will be the case if the
+					// MSB is 1) then the last partial product is the complement of
+					// input a
+					product = product + (a_twos_complement << i);
+				end else begin
+					product = product + ((a_sign_extended << i) & {WIDTH_C{b[i]}});
+				end
 			end
 		end else begin
 			// If it happens that input a has less bits than input b, then
@@ -40,7 +67,14 @@ module multiplier #
 			// b have the same number of bits then it does not matter which is
 			// the multiplier and which is the multiplicand.
 			for (i = 0; i < WIDTH_A; i = i + 1) begin
-				product = product + ((b & {WIDTH_B{a[i]}}) << i);
+				if ((i == (WIDTH_A-1)) & a_is_negative) begin
+					// If a is a negative quantity (which will be the case if the
+					// MSB is 1) then the last partial product is the complement of
+					// input b
+					product = product + (b_twos_complement << i);
+				end else begin
+					product = product + ((b_sign_extended << i) & {WIDTH_C{a[i]}});
+				end
 			end
 		end
 	end
